79fb001a162f4006b97022193a9859045d4d58cd
James Sadler <freshtonic@gmail.com>
2017-04-27 11:13:41 +1000

    Add deploy script
    This also sets the ACL of the uploaded content so that it doesn't need
    to be made public via the AWS console after each upload.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x -e
+
+aws s3 cp . s3://alembic.com.au \
+	--acl public-read \
+	--recursive \
+	--exclude "*" \
+	--include assets \
+	--include favicon.ico \
+	--include index.html
+		
