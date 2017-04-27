c94b3663434c5481b35ffc70eea2abe6b276e07c
James Sadler <freshtonic@gmail.com>
2017-04-27 21:38:56 +1000

    Bust the CSS cache until use a build system
    - also make deploy.sh actually recurse upload the assets. WTF S3?!

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ aws s3 cp . s3://alembic.com.au \
 	--acl public-read \
 	--recursive \
 	--exclude "*" \
-	--include assets \
+	--include "assets/**" \
 	--include favicon.ico \
 	--include index.html
 		
