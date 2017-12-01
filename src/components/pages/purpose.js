54c1913071144311fab1c074c9b6b1a30bdacc27
Luke Rollans <hello@lukerollans.me>
2017-12-01 14:33:31 +0800

    Add some "primary article" styling

--- src/components/pages/purpose.js
+++ src/components/pages/purpose.js
@@ -1,34 +1,40 @@
 import React from "react"
 import PrimaryHeader from "../primary-header"
+import Article from "../article"
 
 export default props => (
   <div className="purpose">
     <PrimaryHeader internal />
 
-    <article>
-      <h1>The Future</h1>
+    <div className="wrap">
+      <Article>
+        <h1>
+          We're Alembic, and this is our{" "}
+          <a href="http://slashpurpose.org">/purpose</a>
+        </h1>
 
-      <p>
-        We believe the future of the web is realtime. It's here now, but not yet
-        evenly distributed
-      </p>
+        <p>
+          We believe the future of the web is realtime. It's here now, but not
+          yet evenly distributed
+        </p>
 
-      <p>
-        We are no longer constrained by the static documents of the first and
-        second wave web. HTTP/2 enables always-on conections and living
-        documents and applications constantly connected and updated with the
-        current shared state of the living web.
-      </p>
+        <p>
+          We are no longer constrained by the static documents of the first and
+          second wave web. HTTP/2 enables always-on conections and living
+          documents and applications constantly connected and updated with the
+          current shared state of the living web.
+        </p>
 
-      <h2>Are you ready?</h2>
+        <h2>Are you ready?</h2>
 
-      <p>
-        The problem we see starting to occur is that if you have thousands or
-        millions of clients constantly connected to your servers, most modern
-        web technology start falling down. Ruby, Node, even Java and .NET
-        platforms aren't highly optimised for those numbers of connections.
-        Elixir, Erlang and Phoenix handle these volumes with ease.
-      </p>
-    </article>
+        <p>
+          The problem we see starting to occur is that if you have thousands or
+          millions of clients constantly connected to your servers, most modern
+          web technology start falling down. Ruby, Node, even Java and .NET
+          platforms aren't highly optimised for those numbers of connections.
+          Elixir, Erlang and Phoenix handle these volumes with ease.
+        </p>
+      </Article>
+    </div>
   </div>
 )
