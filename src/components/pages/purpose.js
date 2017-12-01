cbc9f151f8251566ee63858ea9cfe8388f128b85
Luke Rollans <hello@lukerollans.me>
2017-12-01 15:24:36 +0800

    Rewrite Purpose copy

--- src/components/pages/purpose.js
+++ src/components/pages/purpose.js
@@ -14,25 +14,71 @@ export default props => (
         </h1>
 
         <p>
-          We believe the future of the web is realtime. It's here now, but not
-          yet evenly distributed
+          We want to build web-based software that empowers our clients and
+          enriches the lives of their users. We want to relentlessly improve
+          ourselves, which in turn, helps us improve the services we offer to
+          our clients and their users.
+        </p>
+
+        <h2>Attainment</h2>
+        <p>
+          We want to ensure all people of Alembic thrive both professionally and
+          personally. This includes a clear distinction between work time and
+          personal time. We want to look forward to coming to work, while also
+          wanting to look forward to rest and relaxation.
         </p>
 
         <p>
-          We are no longer constrained by the static documents of the first and
-          second wave web. HTTP/2 enables always-on conections and living
-          documents and applications constantly connected and updated with the
-          current shared state of the living web.
+          While at work, we should want the work we're doing to be meaningful
+          and life embettering.
         </p>
 
-        <h2>Are you ready?</h2>
+        <h2>Inclusivity</h2>
+        <p>
+          We want to maintain our environment of inclusion and equality. All
+          humans are equal.
+        </p>
+
+        <h2>Quality</h2>
+        <p>
+          We want the software we build and design to be of very high quality.
+          We define quality as
+        </p>
+
+        <ul>
+          <li>Maintainable.</li>
+          <li>Well tested.</li>
+          <li>Scalable.</li>
+          <li>Real-time.</li>
+          <li>Continuously integrated.</li>
+          <li>Simple, minimalistic and understandable.</li>
+        </ul>
 
         <p>
-          The problem we see starting to occur is that if you have thousands or
-          millions of clients constantly connected to your servers, most modern
-          web technology start falling down. Ruby, Node, even Java and .NET
-          platforms aren't highly optimised for those numbers of connections.
-          Elixir, Erlang and Phoenix handle these volumes with ease.
+          Furthermore, we strive to continuously improve software development
+          for our clients. We want them to be as happy to be working with us as
+          we are happy to be building software for them.
+        </p>
+
+        <h2>Trust</h2>
+        <p>
+          We want our colleagues to share our relentless desire to improve, our
+          unquenchable curiosity, our insatiable appetite for better. We should
+          be open, supportive and an asset to those we work with. We should be
+          each others cheer squad.
+        </p>
+
+        <h2>Longevity</h2>
+        <p>
+          We want to form long term, fruitful relationships. Success is a
+          marathon, not a sprint. We strive to never betray our values for
+          short-term profit.
+        </p>
+
+        <p>
+          We want to choose the tools of the future. We will not blindly take
+          orders and cash cheques, but truly collaborate and deliver real
+          solutions.
         </p>
       </Article>
     </div>
