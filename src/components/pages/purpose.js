0d691c866423dad906e833c08d5a1399926e918b
Josh Price <josh@alembic.com.au>
2017-12-02 15:27:15 +1000

    Change some wording, remove extraneous punctuation

--- src/components/pages/purpose.js
+++ src/components/pages/purpose.js
@@ -10,48 +10,53 @@ export default props => (
       <Article>
         <h1>
           We're Alembic, and this is our{" "}
-          <a href="http://slashpurpose.org">/purpose</a>
+          <a href="http://slashpurpose.org">purpose</a>
         </h1>
 
         <p>
-          We want to build web-based software that empowers our clients and
-          enriches the lives of their users. We want to relentlessly improve
+          We want to build software that empowers our clients and
+          enriches the lives of their customers. We want to relentlessly improve
           ourselves, which in turn, helps us improve the services we offer to
-          our clients and their users.
+          our clients and their customers.
         </p>
 
-        <h2>Attainment</h2>
+        <h2>Learning and Enjoyment</h2>
         <p>
           We want to ensure all people of Alembic thrive both professionally and
           personally. This includes a clear distinction between work time and
           personal time. We want to look forward to coming to work, while also
-          wanting to look forward to rest and relaxation.
+          enjoying rest and relaxation with our families.
         </p>
 
         <p>
-          While at work, we should want the work we're doing to be meaningful
-          and life embettering.
+          While at work, we should love the work we're doing. We need our work
+          to be meaningful and to make a difference in the world around us.
         </p>
 
         <h2>Inclusivity</h2>
         <p>
-          We want to maintain our environment of inclusion and equality. All
-          humans are equal.
+          We want to foster an environment of inclusion and equality.
+          We believe that all humans are fundamentally equal, but bring
+          different, and incredible qualities to the table.
         </p>
 
         <h2>Quality</h2>
         <p>
           We want the software we build and design to be of very high quality.
-          We define quality as
+          We define quality in terms of:
         </p>
 
         <ul>
-          <li>Maintainable.</li>
-          <li>Well tested.</li>
-          <li>Scalable.</li>
-          <li>Real-time.</li>
-          <li>Continuously integrated.</li>
-          <li>Simple, minimalistic and understandable.</li>
+          <li>Simplicity</li>
+          <li>Human centred</li>
+          <li>Minimalism</li>
+          <li>Integrity</li>
+          <li>Correctness</li>
+          <li>Scalability</li>
+          <li>Ease of change</li>
+          <li>Accessibility</li>
+          <li>Understandable</li>
+          <li>Friendliness</li>
         </ul>
 
         <p>
@@ -63,22 +68,17 @@ export default props => (
         <h2>Trust</h2>
         <p>
           We want our colleagues to share our relentless desire to improve, our
-          unquenchable curiosity, our insatiable appetite for better. We should
-          be open, supportive and an asset to those we work with. We should be
-          each others cheer squad.
+          unquenchable curiosity, our insatiable desire for finding a better way.
+          We should be open, supportive and an asset to those we work with.
+          We should be each others' cheer squad.
         </p>
 
         <h2>Longevity</h2>
         <p>
-          We want to form long term, fruitful relationships. Success is a
-          marathon, not a sprint. We strive to never betray our values for
-          short-term profit.
-        </p>
-
-        <p>
-          We want to choose the tools of the future. We will not blindly take
-          orders and cash cheques, but truly collaborate and deliver real
-          solutions.
+          We want to form long-term, fruitful relationships.
+          Success is a marathon, not a sprint.
+          We strive to never betray our core values for short-term profit.
+          We also want to choose the tools that last the test of time.
         </p>
       </Article>
     </div>
